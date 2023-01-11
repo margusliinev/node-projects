8fb0842bc1314cc930024c9cdbc24dc91ff7bc1c
Margus Liinev <margus.liinev@gmail.com>
2023-01-11 23:33:36 +0200

    parameters and query strings setup

--- 01-tutorial/app.js
+++ 01-tutorial/app.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const { products } = require('./data');
 
-app.use(express.static('./public'));
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image };
+    });
+    res.json(newProducts);
+});
+
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params;
+    console.log(req.params);
+    const singleProduct = products.find((product) => {
+        if (product.id === Number(productID)) {
+            return product;
+        }
+    });
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist!');
+    }
+    return res.json(singleProduct);
+});
+
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        });
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length < 1) {
+        return res.status(200).json({ success: true, data: [] });
+    }
+    return res.status(200).json(sortedProducts);
+});
 
 app.all('*', (req, res) => {
     res.status(404).send('Resource not found');

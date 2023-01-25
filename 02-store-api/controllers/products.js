31b4e7611326a6792fabe014932d2109043e5eff
Margus Liinev <margus.liinev@gmail.com>
2023-01-25 20:41:45 +0200

    products filtering

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -10,7 +10,20 @@ async function getAllProductsStatic(req, res) {
 }
 
 async function getAllProducts(req, res) {
-    res.status(200).json({ msg: 'Products route' });
+    console.log(req.query);
+    const { featured, company, name } = req.query;
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = name;
+    }
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 module.exports = { welcomePage, getAllProductsStatic, getAllProducts };

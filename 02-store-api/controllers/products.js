469f154250a5827c0572e33f6004c317d439a2f1
Margus Liinev <margus.liinev@gmail.com>
2023-05-07 23:23:37 +0300

    added update, delete and get single product operations

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -18,4 +18,37 @@ const createProduct = async (req, res) => {
     }
 };
 
-module.exports = { getAllProducts, createProduct };
+const getSingleProduct = async (req, res) => {
+    try {
+        const product = await db.query('select * from products where id = $1', [req.params.id]);
+        res.status(200).json({ product });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+};
+
+const updateProduct = async (req, res) => {
+    try {
+        const product = await db.query('update products set name = $1, price = $2, company = $3, rating = $4 where id = $5 returning *', [
+            req.body.name,
+            req.body.price,
+            req.body.company,
+            req.body.rating,
+            req.params.id,
+        ]);
+        res.status(200).json({ product });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await db.query('delete from products where id = $1', [req.params.id]);
+        res.status(204).json({ product });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+};
+
+module.exports = { getAllProducts, createProduct, getSingleProduct, updateProduct, deleteProduct };
--- 02-store-api/routes/products.js
+++ 02-store-api/routes/products.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllProducts, createProduct } = require('../controllers/products');
+const { getAllProducts, createProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/products');
 
 router.route('/').get(getAllProducts).post(createProduct);
+router.route('/:id').get(getSingleProduct).put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
